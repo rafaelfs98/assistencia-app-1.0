f38c61e16ef5e118ff809d2c26f9b66a4bc94e7e
rafaelfs98 <rafaelfs98@outlook.com>
2023-03-25 17:15:42 -0300

    bash: linha 1: :wq: comando não encontrado Add context

--- src/Components/Layout/Header/index.tsx
+++ src/Components/Layout/Header/index.tsx
@@ -1,13 +1,45 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 
-import { Alert, Header } from "@mantine/core";
+import {
+  Alert,
+  Burger,
+  Header,
+  MediaQuery,
+  useMantineTheme,
+} from "@mantine/core";
 import Breadcrumb from "../Breadcrumb";
+import NavbarContext, { NavbarTypes } from "../../../Context/Navbarcontext";
 
 const HeaderApp: React.FC = () => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState<boolean>(false);
+  const [state, dispatch] = useContext(NavbarContext);
+
+  const NavbarOpened = useCallback(() => {
+    dispatch({
+      payload: opened,
+      type: NavbarTypes.SET_NAVBAR_VISIBLE,
+    });
+  }, []);
+
+  useEffect(() => {
+    NavbarOpened();
+  }, [opened]);
+
   return (
     <Header height={{ base: 50, md: 70 }} p="md">
       <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <Breadcrumb />
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <Burger
+            opened={state.navBarVisible}
+            onClick={() => {
+              setOpened((opened: boolean) => !opened);
+            }}
+            size="sm"
+            color={theme.colors.gray[6]}
+            mr="xl"
+          />
+        </MediaQuery>
       </div>
     </Header>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { MantineProvider, Text } from "@mantine/core";
 import { theme } from "./theme";
 import AppRouter from "./AppRouter";
+import { NavbarContextProvider } from "./Context/Navbarcontext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <AppRouter />
+      <NavbarContextProvider>
+        <AppRouter />
+      </NavbarContextProvider>
     </MantineProvider>
   </StrictMode>
 );
