bc6c0c53d1f3e6db793c81bb7cdfb131a099c1be
rafaelfs98 <rafaelfs98@outlook.com>
2023-04-08 14:34:08 -0300

    customer page migration to new subbase

--- src/Pages/Cadastro/Clientes/ClientesOutlet.tsx
+++ src/Pages/Cadastro/Clientes/ClientesOutlet.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import { useSupabase } from "../../../hooks/useSupabase";
-import { supabase } from "../../../services/supabase/supabaseClient";
 import { ClientesFormData } from "../../../services/Types";
-
-type ClienteOutletContextType = {
-  cliente: ClientesFormData[];
-};
 
 const ClientesOutlet = () => {
   const { clienteId } = useParams<{ clienteId: string }>();
 
   const { data: cliente, mutate: mutateCliente } =
     useSupabase<ClientesFormData>({
-      table: "clientes",
-      order: "id",
-      ascending: true,
-      eq: { value: "id", id: clienteId as string },
+      uri: `/clientes?id=eq.${clienteId}`,
     });
 
   if (cliente) {
