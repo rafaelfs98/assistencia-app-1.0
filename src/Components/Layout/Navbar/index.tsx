e1e65a1573383150fd9fafdad74743fb37627fae
rafaelfs98 <rafaelfs98@outlook.com>
2023-04-25 20:31:15 -0300

    footer 1.0

--- src/Components/Layout/Navbar/index.tsx
+++ src/Components/Layout/Navbar/index.tsx
@@ -1,5 +1,6 @@
 import {
   Divider,
+  MediaQuery,
   Menu,
   Navbar,
   NavLink,
@@ -45,79 +46,81 @@ const NavbarApp = () => {
   };
 
   return (
-    <Navbar width={{ base: 80 }} p="md">
-      <Navbar.Section p="xs">
-        <UnstyledButton>Suite os</UnstyledButton>
-      </Navbar.Section>
-      <Divider my="sm" />
-      <Navbar.Section grow>
-        <Stack justify="center" spacing={0}>
-          <NavLink
-            title="Ordem de Servicos"
-            label="Ordem de Sevicos"
-            icon={<IconClipboardList size="1.5rem" stroke={1.5} />}
-          />
+    <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+      <Navbar hiddenBreakpoint="xs" p="md" width={{ base: 80 }}>
+        <Navbar.Section p="xs">
+          <UnstyledButton>Suite os</UnstyledButton>
+        </Navbar.Section>
+        <Divider my="sm" />
+        <Navbar.Section grow>
+          <Stack justify="center" spacing={0}>
+            <NavLink
+              title="Ordem de Servicos"
+              label="Ordem de Sevicos"
+              icon={<IconClipboardList size="1.5rem" stroke={1.5} />}
+            />
 
-          <NavLink
-            title="Orcamento"
-            label="Orcamento"
-            icon={<IconCoin size="1.5rem" stroke={1.5} />}
-          />
-        </Stack>
-      </Navbar.Section>
-      <Navbar.Section>
-        <Stack justify="center" spacing={0}>
-          <Menu shadow="md" width={150} position="left-end">
-            <Menu.Target>
-              <NavLink
-                title="Gerencial"
-                label="Gerencial"
-                icon={<IconArticle size="1.5rem" stroke={1.5} />}
-              />
-            </Menu.Target>
+            <NavLink
+              title="Orcamento"
+              label="Orcamento"
+              icon={<IconCoin size="1.5rem" stroke={1.5} />}
+            />
+          </Stack>
+        </Navbar.Section>
+        <Navbar.Section>
+          <Stack justify="center" spacing={0}>
+            <Menu shadow="md" width={150} position="left-end">
+              <Menu.Target>
+                <NavLink
+                  title="Gerencial"
+                  label="Gerencial"
+                  icon={<IconArticle size="1.5rem" stroke={1.5} />}
+                />
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <Menu.Label>Cadastros</Menu.Label>
+              <Menu.Dropdown>
+                <Menu.Label>Cadastros</Menu.Label>
 
-              <Menu.Item
-                title="Clientes"
-                icon={<IconUsers size="1rem" stroke={1.5} />}
-                onClick={() => navigateTo("clientes")}
-              >
-                Clientes
-              </Menu.Item>
-              <Menu.Item
-                title="Servicos"
-                icon={<IconTool size="1rem" stroke={1.5} />}
-                onClick={() => navigateTo("servicos")}
-              >
-                Servicos
-              </Menu.Item>
-              <Menu.Item
-                title="Status"
-                icon={<IconHierarchy size="1rem" stroke={1.5} />}
-                onClick={() => navigateTo("status")}
-              >
-                Status
-              </Menu.Item>
-              <Menu.Item
-                title="Formas de Pagamentos"
-                icon={<IconCash size="1.5rem" stroke={1.5} />}
-              >
-                Formas de Pagamentos
-              </Menu.Item>
-              <Menu.Divider />
-              <Menu.Label>Gerencial</Menu.Label>
-              <Menu.Item icon={<IconSettings size={14} />}>
-                Configuracao de Relatorio
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-          <Divider my="sm" />
-          {<UserAvatar />}
-        </Stack>
-      </Navbar.Section>
-    </Navbar>
+                <Menu.Item
+                  title="Clientes"
+                  icon={<IconUsers size="1rem" stroke={1.5} />}
+                  onClick={() => navigateTo("clientes")}
+                >
+                  Clientes
+                </Menu.Item>
+                <Menu.Item
+                  title="Servicos"
+                  icon={<IconTool size="1rem" stroke={1.5} />}
+                  onClick={() => navigateTo("servicos")}
+                >
+                  Servicos
+                </Menu.Item>
+                <Menu.Item
+                  title="Status"
+                  icon={<IconHierarchy size="1rem" stroke={1.5} />}
+                  onClick={() => navigateTo("status")}
+                >
+                  Status
+                </Menu.Item>
+                <Menu.Item
+                  title="Formas de Pagamentos"
+                  icon={<IconCash size="1.5rem" stroke={1.5} />}
+                >
+                  Formas de Pagamentos
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Label>Gerencial</Menu.Label>
+                <Menu.Item icon={<IconSettings size={14} />}>
+                  Configuracao de Relatorio
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+            <Divider my="sm" />
+            {<UserAvatar />}
+          </Stack>
+        </Navbar.Section>
+      </Navbar>
+    </MediaQuery>
   );
 };
 
--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -1,13 +1,40 @@
-import { AppShell, useMantineTheme } from "@mantine/core";
-import { useEffect } from "react";
+import {
+  AppShell,
+  Divider,
+  Footer,
+  Group,
+  MediaQuery,
+  Menu,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
+import { useEffect, useId, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { supabase } from "../../services/supabase/supabaseClient";
 import HeaderApp from "./Header";
 import NavbarApp from "./Navbar";
+import UserAvatar from "./UserAvatar";
+
+import {
+  IconArticle,
+  IconCash,
+  IconClipboardList,
+  IconCoin,
+  IconHierarchy,
+  IconSettings,
+  IconTool,
+  IconUsers,
+} from "@tabler/icons-react";
+import { useMediaQuery } from "@mantine/hooks";
 
 export default function AppShellDemo() {
   const theme = useMantineTheme();
   const navigate = useNavigate();
+  const [opened, setOpened] = useState<String>();
+
+  const navigateTo = (resource: string) => {
+    navigate(resource);
+  };
 
   useEffect(() => {
     const getLoggedInUser = async () => {
@@ -25,18 +52,80 @@ export default function AppShellDemo() {
 
   return (
     <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
+      navbarOffsetBreakpoint="sm"
       layout="alt"
-      fixed
       navbar={<NavbarApp />}
-      header={<HeaderApp />}
+      footer={
+        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+          <footer>
+            <Group position="apart">
+              <Group>
+                <UnstyledButton>Suite os</UnstyledButton>
+              </Group>
+
+              <Group position="apart">
+                <UnstyledButton ml="md">
+                  <IconClipboardList size="1.5rem" stroke={1.5} />
+                </UnstyledButton>
+                <UnstyledButton ml="md">
+                  <IconCoin size="1.5rem" stroke={1.5} />
+                </UnstyledButton>
+
+                <Menu shadow="md" width={150} position="top-start">
+                  <Menu.Target>
+                    <UnstyledButton ml="md">
+                      {" "}
+                      <IconArticle size="1.5rem" stroke={1.5} />
+                    </UnstyledButton>
+                  </Menu.Target>
+
+                  <Menu.Dropdown>
+                    <Menu.Label>Cadastros</Menu.Label>
+
+                    <Menu.Item
+                      title="Clientes"
+                      icon={<IconUsers size="1rem" stroke={1.5} />}
+                      onClick={() => navigateTo("clientes")}
+                    >
+                      Clientes
+                    </Menu.Item>
+                    <Menu.Item
+                      title="Servicos"
+                      icon={<IconTool size="1rem" stroke={1.5} />}
+                      onClick={() => navigateTo("servicos")}
+                    >
+                      Servicos
+                    </Menu.Item>
+                    <Menu.Item
+                      title="Status"
+                      icon={<IconHierarchy size="1rem" stroke={1.5} />}
+                      onClick={() => navigateTo("status")}
+                    >
+                      Status
+                    </Menu.Item>
+                    <Menu.Item
+                      title="Formas de Pagamentos"
+                      icon={<IconCash size="1.5rem" stroke={1.5} />}
+                    >
+                      Formas de Pagamentos
+                    </Menu.Item>
+                    <Menu.Divider />
+                    <Menu.Label>Gerencial</Menu.Label>
+                    <Menu.Item icon={<IconSettings size={14} />}>
+                      Configuracao de Relatorio
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Group>
+
+              <Group>
+                <Divider orientation="vertical" />
+                <UserAvatar />
+              </Group>
+            </Group>
+          </footer>
+        </MediaQuery>
+      }
     >
       <Outlet />
     </AppShell>
