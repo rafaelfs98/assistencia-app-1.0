ae60773262d8ef08dffa35535b73e6484c748e6c
rafaelfs98 <rafaelfs98@outlook.com>
2023-06-08 14:41:38 -0300

    #118, #119 , #117 Creation and update screen

---
+++ src/Pages/Cadastro/FormasDePagamentos/paymentMethodsForm.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Container, TextInput, Title } from "@mantine/core";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useOutletContext, useParams } from "react-router-dom";
+import { KeyedMutator } from "swr";
+import useFormActions from "../../../hooks/useFormActions";
+import { paymentMethodsUpsert } from "../../../services/PaymentMethods";
+import { PaymentMethodData } from "../../../services/Types/suiteOS";
+
+const PaymentMethodForm = () => {
+  const { paymentmethodId } = useParams();
+
+  const [title, setTitle] = useState<String>("Adicionar Forma de Pagamneto");
+
+  const context = useOutletContext<{
+    paymentMethod: PaymentMethodData[];
+    mutatePaymentMethod: KeyedMutator<PaymentMethodData>;
+  }>();
+
+  const {
+    form: { onError, onSave, onClose },
+  } = useFormActions();
+
+  const { handleSubmit, register } = useForm<PaymentMethodData>({
+    defaultValues: context ? context?.paymentMethod[0] : {},
+  });
+
+  const onSubmit = async (form: PaymentMethodData) => {
+    const { error } = await paymentMethodsUpsert(form, Number(paymentmethodId));
+
+    if (!error) {
+      return onSave();
+    }
+
+    return onError(error.message);
+  };
+
+  useEffect(() => {
+    if (context?.paymentMethod) {
+      document.title = `${context?.paymentMethod?.map((item) => item.name)}`;
+      setTitle("Editar Forma de Pagamento");
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Box>
+        <Title order={4}>{title}</Title>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextInput
+            mt="md"
+            required
+            label="Forma de Pagemento"
+            {...register("name")}
+          />
+          <Button.Group mt="lg">
+            <div>
+              <Button type="submit" mt="md">
+                Submit
+              </Button>
+            </div>
+            <Button mt="md" ml="sm" color="gray" onClick={onClose}>
+              Close
+            </Button>
+          </Button.Group>
+        </form>
+      </Box>
+    </Container>
+  );
+};
+export default PaymentMethodForm;
