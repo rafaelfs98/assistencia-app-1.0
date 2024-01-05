9c9de552fe292ceb9256119cdb95009fcf8fb487
rafaelfs98 <rafaelfs98@outlook.com>
2024-01-05 08:55:03 -0300

    add example.env

--- src/services/supabase/supabaseClient.ts
+++ src/services/supabase/supabaseClient.ts
@@ -1,10 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../Types/supabase";
 
-const supabaseUrl = "https://senbcwzimnbyqbwsfect.supabase.co";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-console.log(supabaseUrl);
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlbmJjd3ppbW5ieXFid3NmZWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk5MzU0NDQsImV4cCI6MTk5NTUxMTQ0NH0.lASI5s6w6eVe8MXnpZ3iIOc0-wYxAIccUjxLGccW7Hw";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
